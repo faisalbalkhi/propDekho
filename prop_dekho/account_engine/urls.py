e0cca43ff84b29e8f18cbeeeeb945286ef39d74a
asthabansal11 <asthabansal11@gmail.com>
2023-03-17 13:10:06 +0530

    Agent page updated

--- prop_dekho/account_engine/urls.py
+++ prop_dekho/account_engine/urls.py
@@ -10,12 +10,5 @@ urlpatterns = [
     path('dashboard-add-listing/', views.dashboard_add_listing, name='dashboard_add_listing'),
     path('contacts/', views.contacts, name='contacts'),
     path('about/', views.about, name='about'),
-<<<<<<< HEAD
     path('pg-Guest/',views.pg_Guest, name='pg_Guest'),
-=======
-    path('agent-list/', views.agent_list, name='agent_list'),
-    path('dashboard-agent/', views.dashboard_agent, name='dashboard_agent'),
-    path('dashboard-myprofile/', views.dashboard_mypropfile, name='dashboard_mypropfile'),
-
->>>>>>> 19d5d10106b8bdc49faa0e02991229494e618b3b
 ]
--- prop_dekho/account_engine/views.py
+++ prop_dekho/account_engine/views.py
@@ -84,21 +84,8 @@ def about(request):
     return render(request, 'about.html')
 
 
-<<<<<<< HEAD
 def pg_Guest(request):
     return render(request, 'pgGuest.html')
 
 
-=======
-def dashboard_agent(request):
-    return render(request, 'dashboard-agents.html')
-
-
-def dashboard_mypropfile(request):
-    return render(request, 'dashboard-myprofile.html')
-
-
-def agent_list(request):
-    return render(request, 'agent-list.html')
->>>>>>> 19d5d10106b8bdc49faa0e02991229494e618b3b
 
